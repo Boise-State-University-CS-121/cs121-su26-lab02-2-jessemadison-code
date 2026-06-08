6c08de062ec9d4d8802a6aa3a39a5c3b310f16d4
Jesse Madison <jessemadison@u.boisestate.edu>
2026-06-07 21:42:17 -0600

    Finishg Lab Activity 3

--- SecondsConverter/SecondsConverter.java
+++ SecondsConverter/SecondsConverter.java
@@ -7,19 +7,22 @@ public class SecondsConverter
     {
         Scanner kbd = new Scanner(System.in);
         {
-        
 
         System.out.print("Enter the number of seconds: ");
         int totalSeconds = kbd.nextInt();
 
         int userHours = totalSeconds / 3600;
-        int userMinutes = totalSeconds / 60;
+        int remainingSeconds = totalSeconds % 3600;
+        int userMinutes = remainingSeconds / 60;
+        int userSeconds = remainingSeconds % 60;
+
+        System.out.println(" ");
 
         System.out.println("Hours: " + userHours);
 
         System.out.println("Minutes: " + userMinutes);
 
-
+        System.out.println("Seconds: " + userSeconds);
 
         /* end the scanner here */
         kbd.close();
