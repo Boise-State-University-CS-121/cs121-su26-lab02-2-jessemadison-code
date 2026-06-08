f3faca56c377341ae927aadba9f59e6b2eb5560b
Jesse Madison <jessemadison@u.boisestate.edu>
2026-06-07 21:23:55 -0600

    Finishing lab activity 2

---
+++ HMSConverter/HMSConverter.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class HMSConverter
+{
+
+    public static void main(String [] args)
+    {
+        Scanner kbd = new Scanner(System.in);
+        {
+
+        System.out.print("Enter the number of hours: ");
+        int userHours = kbd.nextInt();
+
+        System.out.print("Enter the number of minutes: ");
+        int userMinutes = kbd.nextInt();
+        
+        System.out.print("Enter the number of seconds: ");
+        int userSeconds = kbd.nextInt();
+
+        int userActHours = userHours * 3600;
+        int userActMinutes = userMinutes * 60;
+
+        int totalSeconds = (userActHours + userActMinutes + userSeconds);
+
+        System.out.println("Total seconds: " + totalSeconds);
+
+        /* end the scanner here */
+        kbd.close();
+        }
+    }
+}
